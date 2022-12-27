dc963b2d5e1e3a0890239839b0d4cb9081b95e36
Saikumar Molleti <80804527+saikumarmolleti@users.noreply.github.com>
2022-12-28 00:16:06 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,5 +1,18 @@
-function letterCombinations(input_digit) {
+function letterCombinations(input_digits) {
+    if(input_digits.length==0) return[];
+    const map = { '2' : 'abc', '3' : 'def', '4' : 'ghi',
+                   '5' : 'jkl', '6' : ',mno', '7' : 'pqrs',
+                   '8' : 'tuv', '9' : 'wxyz',};
+           const queue=[''];
+           for(const digit of input_digits){
+             const len = queue.length;
+             for( let i=0; i<len; i++) {
+              const current = queue.shift();
+              map[digit].split('').forEach(i => queue.push(current+i));
+             }
+           }
+           return queue
   //Complete the function
-}
+};
 
 module.exports = letterCombinations;
